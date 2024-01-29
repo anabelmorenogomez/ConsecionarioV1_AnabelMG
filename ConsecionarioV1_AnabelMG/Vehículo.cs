2f2d2f75678e2c1654740e8cd5a46697d5311a55
anabelmorenogomez <amorenogomez5@hotmail.com>
2024-01-29 12:37:14 +0100

    Clase Vehiculo: Miembros privados, propiedades y constructores

--- ConsecionarioV1_AnabelMG/Vehículo.cs
+++ ConsecionarioV1_AnabelMG/Vehículo.cs
@@ -11,11 +11,31 @@ namespace ConsecionarioV1_AnabelMG
         // Constantes 
 
         // Miembros Privados 
+        private string _marca;
+        private string _modelo;
 
         // Constructores 
+        public Vehículo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
 
         // Propiedades 
-
+        public string Marca
+        {
+            get { return _marca; }
+            set {
+                // TODO: Proporcionar seguridad y protección al miembro -- Marca
+                _marca = value; }
+        }
+        public string Modelo
+        {
+            get { return _modelo; }
+            set {
+                // TODO: Proporcionar seguridad y protección al miembro -- Modelo
+                _modelo = value; }
+        }
         // Métodos Públicos 
 
         // Métodos Privados 
