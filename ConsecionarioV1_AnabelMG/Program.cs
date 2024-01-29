dcf9be9efb56636e9e610da957fca486f19aee9c
anabelmorenogomez <amorenogomez5@hotmail.com>
2024-01-29 12:04:13 +0100

    Agregar archivos de proyecto.

---
+++ ConsecionarioV1_AnabelMG/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsecionarioV1_AnabelMG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Controlador Principal de la Aplicación 
+
+            // Menú de Acciones 
+
+            // Proceso de las Acciones
+        }
+    }
+}
